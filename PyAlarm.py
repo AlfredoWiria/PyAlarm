d62fceb299c04715ec05e3e006b8415772310376
Alfredo Morgen Wiria <Alfredo7Romero@gmail.com>
2014-08-15 14:48:54 +0700

    ADDED: Simple main menu AND alarm time setting

--- PyAlarm.py
+++ PyAlarm.py
@@ -17,4 +17,52 @@ def alarmClock(hour, minute):
             os.system("shutdown /s /t 180 /c \"Time to Sleep...\"")
             break
 
-alarmClock(21, 0)
+choice = 0
+
+try:
+    with open("alarms.txt") as alarmFile:
+        hour = int(alarmFile.readline())
+        minute = int(alarmFile.readline())
+except FileNotFoundError:
+    hour = -1
+    minute = -1
+
+while choice != 3:
+    print("PyAlarm")
+    print("By: Edo Morgen")
+    print("")
+
+    if hour == -1 and minute == -1:
+        print("Alarm has not been set")
+    else:
+        print("Alarm set for {:02}:{:02}".format(hour, minute))
+    
+    print("1. Turn on alarm")
+    print("2. Configure alarm")
+    print("3. Exit")
+    print("")
+
+    choice = int(input("Choice: "))
+    if choice == 1:
+        if hour == -1 and minute == -1:
+            print("Alarm has not been set")
+            print("Please set it first...")
+        else:
+            alarmClock(hour, minute)
+    elif choice == 2:
+        hour = int(input("Input the hour (in 24-hour format): "))
+        minute = int(input("Input the minute: "))
+
+        alarmFile = open("alarms.txt", mode = 'w')
+        alarmFile.write("{}\n".format(str(hour)))
+        alarmFile.write("{}\n".format(str(minute)))
+        alarmFile.close()
+        
+    elif choice == 3:
+        pass
+    else:
+        print("Invalid input...")
+
+    print("")
+    print("")
+    print("")
